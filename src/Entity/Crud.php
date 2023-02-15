452d5244447208f1fc64ec585a0703c4dad91f03
Marta Punset <martapunset10@gmail.com>
2023-02-15 19:13:34 +0100

    create class/ create form added

--- src/Entity/Crud.php
+++ src/Entity/Crud.php
@@ -17,8 +17,42 @@ class Crud
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Description;
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->Name;
+    }
+
+    public function setName(string $Name): self
+    {
+        $this->Name = $Name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->Description;
+    }
+
+    public function setDescription(string $Description): self
+    {
+        $this->Description = $Description;
+
+        return $this;
     }
 }
